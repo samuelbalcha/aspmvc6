53eb9ef07ad83033c9623e2a2217c0d5fe973e4c
samuel balcha <samuel.balcha@gmail.com>
2016-03-29 20:42:15 +0300

    Implemented SuperHeroController methods

--- src/demo/Startup.cs
+++ src/demo/Startup.cs
@@ -12,8 +12,8 @@ namespace demo
     public class Startup
     {
         public IConfigurationRoot Configuration { get; set; }
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940 
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
--- src/demo/apis/SuperHerosController.cs
+++ src/demo/apis/SuperHerosController.cs
@@ -15,45 +15,81 @@ namespace demo.apis
     [Route("api/[controller]")]
     public class SuperHerosController : Controller
     {
-        
-        private IRepository<SuperHero> _repository { get; set; }
-        
-        public SuperHerosController(IRepository<SuperHero> repo)
+        private readonly IRepository<SuperHero> _repository;
+        private IUnitOfWork _unitOfWork;
+
+        public SuperHerosController(IRepository<SuperHero> repo, IUnitOfWork unitOfWork)
         {
             _repository = repo;
+            _unitOfWork = unitOfWork;
         }
 
-        // GET: api/superhero
+        // GET: api/superheros
         [HttpGet]
         public IEnumerable<SuperHero> Get()
         {
             return _repository.GetAll();
         }
 
-        // GET api/values/5
+        // GET api/superheros/:id
         [HttpGet("{id}")]
-        public SuperHero Get(int id)
+        public IActionResult Get(int id)
         {
-            return _repository.Get(id);
+            var hero = _repository.Get(id);
+            if (hero != null)
+                return new ObjectResult(hero);
+             
+             return new HttpNotFoundResult();
         }
 
-        // POST api/values
+        // POST api/superheros
         [HttpPost]
-        public void Post([FromBody]SuperHero hero)
+        public IActionResult Post([FromBody]SuperHero hero)
         {
-           
+            var exists = _repository.Find(x => x.Name == hero.Name).FirstOrDefault();
+            
+            if (exists == null)
+            {
+                _repository.Add(hero);
+            }
+            else
+            {
+                exists.Name = hero.Name;
+            }
+            _unitOfWork.Complete();
+            return new ObjectResult(hero);
         }
 
-        // PUT api/values/5
+        // PUT api/superheros/:id
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        public IActionResult Put(int id, [FromBody]SuperHero hero)
         {
+            var exists = _repository.Get(id);
+            if (exists != null)
+            {
+                exists.Name = hero.Name;
+            }
+            else
+            {
+                _repository.Add(hero);
+            }
+            _unitOfWork.Complete();
+            return new ObjectResult(hero);
         }
 
-        // DELETE api/values/5
+        // DELETE api/superheros/:id
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(int id)
         {
+            var exists = _repository.Get(id);
+            if(exists != null)
+            {
+                _repository.Remove(exists);
+                _unitOfWork.Complete();
+                return new HttpStatusCodeResult(200);
+            }
+
+            return new HttpStatusCodeResult(404);
         }
     }
 }
